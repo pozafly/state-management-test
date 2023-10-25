1433569508b5e2085b1c15161e7c946a77abbce7
pozafly <pozafly@gmail.com>
2023-10-25 14:04:14 +0900

    chore: add recoil, eslint setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,18 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-hooks', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: 'useRecoilCallback',
+      },
+    ],
   },
-}
+};
