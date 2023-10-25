c172d09834c5fc733fca21c94a9d872803563d06
pozafly <pozafly@gmail.com>
2023-10-25 16:53:48 +0900

    feat: Todo 리스트에 완료 및 삭제 기능 추가

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,17 +1,56 @@
+import { useRecoilState } from 'recoil';
 import type { Todo } from './TodoList.tsx';
+import { todoListState } from '../store/index.ts';
+import type { ChangeEvent } from 'react';
 
 type TodoItemProp = {
   item: Todo;
 };
 
-export default function TodoItem({
-  item: { id, text, isComplete },
-}: TodoItemProp) {
+export default function TodoItem({ item }: TodoItemProp) {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((listItem: Todo) => listItem === item);
+
+  const editItemText = (event: ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: target.value,
+    });
+    setTodoList(newList);
+  };
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    });
+
+    setTodoList(newList);
+  };
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index);
+    setTodoList(newList);
+  };
+
   return (
     <>
-      <div>id: {id}</div>
-      <div>text: {text}</div>
-      <div>isComplete: {isComplete}</div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
     </>
   );
+}
+
+function replaceItemAtIndex<T>(arr: T[], index: number, newValue: T): T[] {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
+
+function removeItemAtIndex<T>(arr: T[], index: number): T[] {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
 }
